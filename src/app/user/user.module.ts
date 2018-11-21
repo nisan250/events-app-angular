d226c156d0131b05e042f41a5e51ea1ae9dd3e8e
nisan250 <nisan250@gmail.com>
2018-11-21 20:24:36 +0000

    adding login shell

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,16 +1,20 @@
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { userRoutes } from './user.routes';
 import { ProfileComponent } from './profile.component';
+import { LoginComponent } from './login.component';
 
 @NgModule({
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild( userRoutes ),
+    FormsModule
   ]
 })
 export class UserModule {}
--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ProfileComponent } from './profile.component';
+import { LoginComponent } from './login.component';
 
 
 export const userRoutes: Routes = [
-  { path: 'profile', component: ProfileComponent}
+  { path: 'profile', component: ProfileComponent},
+  { path: 'login', component: LoginComponent}
 ];
