ed3dcb0362d77662e33b7efbc74b6e448876058f
nisan250 <nisan250@gmail.com>
2018-11-26 17:31:05 +0000

    using the injectable object toaster in demo page

--- docs/main.js
+++ docs/main.js
@@ -400,30 +400,42 @@ module.exports = ".card {\n  border-radius: 6px;\n  box-shadow: 0 2px 2px rgba(2
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "DemoComponent", function() { return DemoComponent; });
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _common_toastr_service__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../common/toastr.service */ "./src/app/common/toastr.service.ts");
 var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (undefined && undefined.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+
 
 // import { ToastrService } from '../common/toastr.service';
 var DemoComponent = /** @class */ (function () {
-    function DemoComponent() {
+    // constructor(private toastrService: ToastrService) { }
+    function DemoComponent(toastr) {
+        this.toastr = toastr;
         this.pageTitle = 'Demo Page - playground area';
     }
-    // constructor(private toastrService: ToastrService) { }
     DemoComponent.prototype.ngOnInit = function () {
     };
     DemoComponent.prototype.handleClick = function () {
         // this.toastrService.success('YOOHOOOO');
+        this.toastr.success('YOOHOOOO');
     };
     DemoComponent = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
             selector: 'aa-demo',
             template: __webpack_require__(/*! ./demo.component.html */ "./src/app/demo/demo.component.html"),
             styles: [__webpack_require__(/*! ./demo.component.scss */ "./src/app/demo/demo.component.scss")]
-        })
+        }),
+        __param(0, Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Inject"])(_common_toastr_service__WEBPACK_IMPORTED_MODULE_1__["TOASTR_TOKEN"])),
+        __metadata("design:paramtypes", [Object])
     ], DemoComponent);
     return DemoComponent;
 }());
--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, InjectionToken, Inject } from '@angular/core';
+import { Toastr, TOASTR_TOKEN } from '../common/toastr.service';
 // import { ToastrService } from '../common/toastr.service';
 
 @Component({
@@ -9,12 +10,13 @@ import { Component, OnInit } from '@angular/core';
 export class DemoComponent implements OnInit {
   pageTitle = 'Demo Page - playground area';
   // constructor(private toastrService: ToastrService) { }
-
+  constructor( @Inject(TOASTR_TOKEN) private toastr: Toastr ) { }
   ngOnInit() {
   }
 
   handleClick() {
     // this.toastrService.success('YOOHOOOO');
+    this.toastr.success('YOOHOOOO');
   }
 }
 
