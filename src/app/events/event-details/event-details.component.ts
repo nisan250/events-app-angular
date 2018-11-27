0f1c546f5b0fc1eb61400d841c22f770a19dd9dd
nisan250 <nisan250@gmail.com>
2018-11-27 18:41:31 +0000

    routing same component - sessions search result now routing correctly

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/event.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { IEvent, ISession } from '../shared/index';
 
 @Component({
@@ -19,7 +19,12 @@ export class EventDetailsComponent implements OnInit {
 
   ngOnInit() {
     // console.log(this.route.snapshot.params['id']);
-    this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
+
+    this.route.params.forEach((params: Params) => {
+      this.event = this.eventService.getEvent(+params['id']);
+    });
+    // good only when i come from different component
+    // this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
   }
 
   // put us in "add" MODE
